fd6c77879d3897003fd3fef0f7b1c992b8f19bf1
NickollMadarriaga <nickoll.madarriaga@udea.edu.co>
2026-03-13 22:55:34 -0500

    creación de 4 funciones para el manejo de las piezas
    Se crearon 4 funciones para el manejo de las piezas, que fueron mover a la derecha e izquierda, la creación de la pieza y la de fijarlas en el tablero cuando ya no se pueden mover mas

---
+++ Funciones.h
@@ -0,0 +1,31 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+
+void crearTablero(unsigned char **&tablero, int alto, int bytesFila);
+
+void liberarTablero(unsigned char **tablero, int alto);
+
+void imprimirTablero(unsigned char **tablero, int alto, int ancho);
+
+bool filaLlena(unsigned char *fila, int bytesFila);
+
+void eliminarFila(unsigned char **tablero, int fila, int alto, int bytesFila);
+
+void limpiarFilas(unsigned char **tablero, int alto, int bytesFila);
+
+void generarPieza(unsigned char pieza[4]);
+
+void rotarPieza(unsigned char pieza[4]);
+
+void moverIzquierda(unsigned char pieza[4]);
+
+void moverDerecha(unsigned char pieza[4]);
+
+bool hayColision(unsigned char **tablero, unsigned char pieza[4],
+                 int posX, int posY, int alto, int ancho);
+
+void fijarPieza(unsigned char **tablero, unsigned char pieza[4],
+                int posX, int posY);
+
+#endif
---
+++ funciones.cpp
@@ -0,0 +1,81 @@
+#include <cstdlib>
+
+void generarPieza(unsigned char pieza[4]) {
+
+    int tipo = rand() % 6;
+
+    switch (tipo) {
+
+    case 0: // Cuadrado
+        pieza[0] = 0b00110000;
+        pieza[1] = 0b00110000;
+        pieza[2] = 0b00000000;
+        pieza[3] = 0b00000000;
+        break;
+
+    case 1: // Línea
+        pieza[0] = 0b11110000;
+        pieza[1] = 0b00000000;
+        pieza[2] = 0b00000000;
+        pieza[3] = 0b00000000;
+        break;
+
+    case 2: // L
+        pieza[0] = 0b10000000;
+        pieza[1] = 0b10000000;
+        pieza[2] = 0b11000000;
+        pieza[3] = 0b00000000;
+        break;
+
+    case 3: // J
+        pieza[0] = 0b01000000;
+        pieza[1] = 0b01000000;
+        pieza[2] = 0b11000000;
+        pieza[3] = 0b00000000;
+        break;
+
+    case 4: // T
+        pieza[0] = 0b11100000;
+        pieza[1] = 0b01000000;
+        pieza[2] = 0b00000000;
+        pieza[3] = 0b00000000;
+        break;
+
+    case 5: // S
+        pieza[0] = 0b01100000;
+        pieza[1] = 0b11000000;
+        pieza[2] = 0b00000000;
+        pieza[3] = 0b00000000;
+        break;
+    }
+}
+
+void moverIzquierda(unsigned char pieza[4]) {
+
+    for (int i = 0; i < 4; i++) {
+        pieza[i] <<= 1;
+    }
+
+}
+void moverDerecha(unsigned char pieza[4]) {
+
+    for (int i = 0; i < 4; i++) {
+        pieza[i] >>= 1;
+    }
+
+}
+void fijarPieza(unsigned char **tablero,
+                unsigned char pieza[4],
+                int posX,
+                int posY) {
+
+    for (int i = 0; i < 4; i++) {
+
+        unsigned char filaPieza = pieza[i] >> posX;
+
+        tablero[posY + i][0] |= filaPieza;
+
+    }
+
+}
+
